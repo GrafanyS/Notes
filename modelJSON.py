52b20e7294104fbea0b87bcc275fc24b9834dde3
GrafanyS <admin@valet.of.by>
2023-02-12 21:41:33 +0300

    ensure_ascii=False

--- modelJSON.py
+++ modelJSON.py
@@ -66,7 +66,7 @@ class ModelJSON(object):
         for note in notes:
             json_strings_list.append({'id': note.note_id, 'date': note.date, 'title': note.title, 'text': note.text})
 
-        notes_json = json.dumps(json_strings_list, indent=4, sort_keys=False, default=str)
+        notes_json = json.dumps(json_strings_list, indent=4, ensure_ascii=False, sort_keys=False, default=str)
 
         with open(self.filename, "w", encoding='utf-8') as my_file:
             my_file.write(notes_json)
