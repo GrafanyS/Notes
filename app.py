557c15bf267bcf26d8f41dba568ef1acf38349c5
GrafanyS <admin@valet.of.by>
2023-02-13 20:10:58 +0300

    программа в бесконечном цикле

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import datetime
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 from controller import Controller
 from modelJSON import ModelJSON
@@ -25,37 +25,38 @@ def run():
             break
 
         if command == '1':
-            print('\nСоздать заметку:')
+            print(Fore.GREEN + '\nСоздать заметку:' + Style.RESET_ALL)
             c.create_note(get_note_data())
 
         elif command == '2':
-            print('\nПрочитать заметку:')
+            print(Fore.GREEN + '\nПрочитать заметку:' + Style.RESET_ALL)
             if c.notes_exist():
                 c.show_note(int(get_number()))
         elif command == '3':
             if c.notes_exist():
-                print('\nОбновить заметку:')
+                print(Fore.YELLOW + '\nОбновить заметку:' + Style.RESET_ALL)
                 updated_id = int(get_number())
                 if c.note_id_exist(updated_id):
                     c.update_note(updated_id, get_note_data())
 
         elif command == '4':
             if c.notes_exist():
-                print('\nУдалить заметку:')
+                print(Fore.RED + '\nУдалить заметку:' + Style.RESET_ALL)
                 delete_id = int(get_number())
                 if c.note_id_exist(delete_id):
                     c.delete_note(delete_id)
 
         elif command == '5':
             if c.notes_exist():
-                print('\nУдалить все заметки:')
-                if input('Вы точно хотите удалить все заметки? (Y/N): ').capitalize() == 'Y':
+                print(Fore.RED + '\nУдалить все заметки:' + Style.RESET_ALL)
+                if input(Fore.RED + 'Вы точно хотите удалить все заметки? (Y/N): '
+                         + Style.RESET_ALL).capitalize() == 'Y':
                     if c.notes_exist():
                         c.delete_all_notes()
 
         elif command == '6':
             if c.notes_exist():
-                print('\nСписок всех заметок:')
+                print(Fore.BLUE + '\nСписок всех заметок:' + Style.RESET_ALL)
                 c.show_notes()
         else:
             print(Fore.RED + 'Команда не найдена' + Style.RESET_ALL)
@@ -75,4 +76,4 @@ def get_number():
         if get_choice.isdigit() and int(get_choice) > 0:
             return get_choice
         else:
-            print('Введите целое положительное число!')
+            print(Fore.RED + 'Введите целое положительное число!' + Style.RESET_ALL)
